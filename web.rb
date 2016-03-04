43f6aca93f59846cf1d895e0d2b93d96e2038c7a
Willis Plummer <willisplummer@gmail.com>
2016-03-04 15:00:14 -0500

    add echo stuff

--- web.rb
+++ web.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'bundler/setup'
 require 'dotenv'
 require 'sinatra'
+require 'alexa_rubykit'
 Dotenv.load
 
 require './api.rb'
-
 
 before do
   content_type('application/json')
@@ -16,5 +16,51 @@ get '/' do
 end
 
 post '/' do
-  "test"
+  # Check that it's a valid Alexa request
+  request_json = JSON.parse(request.body.read.to_s)
+  # Creates a new Request object with the request parameter.
+  request = AlexaRubykit.build_request(request_json)
+
+  # We can capture Session details inside of request.
+  # See session object for more information.
+  session = request.session
+  p session.new?
+  p session.has_attributes?
+  p session.session_id
+  p session.user_defined?
+
+  # We need a response object to respond to the Alexa.
+  response = AlexaRubykit::Response.new
+
+  # We can manipulate the request object.
+  #
+  #p "#{request.to_s}"
+  #p "#{request.request_id}"
+
+  # Response
+  # If it's a launch request
+  if (request.type == 'LaunchRequest')
+    # Process your Launch Request
+    # Call your methods for your application here that process your Launch Request.
+    response.add_speech('Ruby running ready!')
+    response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
+  end
+
+  if (request.type == 'INTENT_REQUEST')
+    # Process your Intent Request
+    p "#{request.slots}"
+    p "#{request.name}"
+    response.add_speech("I received an intent named #{request.name}?")
+    response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{request.name}" } )
+  end
+
+  if (request.type =='SESSION_ENDED_REQUEST')
+    # Wrap up whatever we need to do.
+    p "#{request.type}"
+    p "#{request.reason}"
+    halt 200
+  end
+
+  # Return response
+  response.build_response
 end
