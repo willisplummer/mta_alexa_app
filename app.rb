1a6a0ddcb0617a708720fa8228649f6e6e037e7a
Willis Plummer <willisplummer@gmail.com>
2016-05-04 11:14:58 -0400

    testing

--- app.rb
+++ app.rb
@@ -92,12 +92,8 @@ post '/' do
     p "activation_key: #{t}"
 
     response.add_speech("Please activate your device at mtabustimes.com. Create an account and then enter your unique activation code: #{alexa.activation_key}")
-    response.add_hash_card( { :title => 'Activate your device', :subtitle => 'It is truly lit' } )
+    # response.add_hash_card( { :title => 'Activate your device', :subtitle => 'It is truly lit' } )
     # response.add_card( { :title => 'Activate your device', :subtitle => 'It is truly lit', :content => "Head to mtabustimes.com. Create an account and then enter your unique activation code: #{alexa.activation_key}"} )
-    p "building response"
-    # Return response
-    response.build_response
-    p "built response"
   end
 
   time = Time.now
