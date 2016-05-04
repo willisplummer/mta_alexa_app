2fa6e7fb987b0cff5b8674ba12168aba8669dbfc
Willis Plummer <willisplummer@gmail.com>
2016-05-04 16:07:31 -0400

    add indexes and foreign_keys to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504032830) do
+ActiveRecord::Schema.define(version: 20160504195153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20160504032830) do
     t.integer "user_id"
   end
 
+  add_index "alexas", ["user_id"], name: "index_alexas_on_user_id", using: :btree
+
   create_table "stops", force: :cascade do |t|
     t.string  "name"
     t.integer "mta_stop_id"
@@ -29,9 +31,13 @@ ActiveRecord::Schema.define(version: 20160504032830) do
     t.integer "user_id"
   end
 
+  add_index "stops", ["user_id"], name: "index_stops_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
   end
 
+  add_foreign_key "alexas", "users"
+  add_foreign_key "stops", "users"
 end
